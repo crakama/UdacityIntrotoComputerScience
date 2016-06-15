3595c477b3b4d17d5075e82027242c0374082ec5
crakama <crakama89@gmail.com>
2016-06-15 22:52:41 +0300

    calculate distance, given speed and time

--- speedoflight.py
+++ speedoflight.py
@@ -1,7 +1,19 @@
-# Write Python code to print out how far light travels 
+# Write Python code to print out how far light travels
 # in centimeters in one nanosecond.  Use the variables
-# defined below.    
+# defined below.
 
 speed_of_light = 299792458   # meters per second
 centimeters = 100            # one meter is 100 centimeters
-nanosecond = 1.0/1000000000  # one billionth of a second
+nanosecond = 1.0 / 1000000000  # one billionth of a second
+
+# Speed = Distance/Time
+time = 0.000000001
+distance = 29979245800
+
+
+def calcSpeed(time, distance):
+
+    speed = distance / time   # cm per second
+    print speed
+
+calcSpeed(time, distance)
--- variable.py
+++ variable.py
@@ -1,6 +1,7 @@
-# Given the variables defined here, write Python 
-# code that prints out the distance, in meters, 
-# that light travels in one processor cycle. 
+# Given the variables defined here, write Python
+# code that prints out the distance, in meters,
+# that light travels in one processor cycle.
 
 speed_of_light = 299792458.0
 cycles_per_second = 2700000000.0
+distance = speed_of_light * cycles_per_second
