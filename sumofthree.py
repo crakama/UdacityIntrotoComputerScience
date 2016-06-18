668c673a2943346f5cbaebcb26f4ee08fa1fb46e
crakama <crakama89@gmail.com>
2016-06-18 23:06:05 +0300

    summation

---
+++ sumofthree.py
@@ -0,0 +1,14 @@
+# Define a procedure, sum3, that takes three
+# inputs, and returns the sum of the three
+# input numbers.
+
+
+def numsum(a, b, c):
+    summation = a + b + c
+    return summation
+
+a = 6
+b = 3
+c = 4
+
+numsum(a, b, c)
