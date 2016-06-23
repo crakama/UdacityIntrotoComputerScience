93fa81838246bda99b7cc75fa305b88994af2f36
crakama <crakama89@gmail.com>
2016-06-23 23:58:58 +0300

    How to manage data; find element

---
+++ findelement.py
@@ -0,0 +1,18 @@
+# Define a procedure, find_element,
+# that takes as its inputs a list
+# and a value of any type, and
+# returns the index of the first
+# element in the input list that
+# matches the value.
+
+# If there is no matching element,
+# return -1.
+
+
+
+
+def find_element(p,t):
+    i = 0
+    while i < len(p):
+        if p[i] == t:
+    return i
