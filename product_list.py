2281e9eb0f99f073f2cdd38e5d477c511ceac58f
crakama <crakama89@gmail.com>
2016-06-25 20:54:53 +0300

    Product list

---
+++ product_list.py
@@ -0,0 +1,26 @@
+# Define a procedure, product_list,
+# takes as input a list of numbers,
+# and returns a number that is
+# the result of multiplying all
+# those numbers together.
+
+
+def product_list(lst):
+    mult = 1
+
+    for i in lst:
+        mult = mult * i
+    print mult
+
+num = [1, 2, 3, 4]
+product_list(num)
+
+
+# print product_list([9])
+#>>> 9
+
+# print product_list([1,2,3,4])
+#>>> 24
+
+# print product_list([])
+#>>> 1
